986c6436ae0dbaa99500c68b9b114e377acc5052
Rick Henry <fredericmhenry@gmail.com>
2017-07-15 02:32:12 -0400

    it mostly works
    Signed-off-by: Rick Henry <fredericmhenry@gmail.com>

--- parse_text.py
+++ parse_text.py
@@ -1,9 +1,10 @@
 #!/usr/bin/env python2
-my_input = { 'body': 'this is a test. This is the text to insert.' }
+input_data = { 'body': 'this is a test. This is the text to insert.',
+        }
 output = { 'test_worked': False, 'insert_text': '' }
 import re
 # if input.get('body') and re.match(r'THIS IS A TEST.', input['body'], re.IGNORECASE):
-if re.match(r'THIS IS A TEST.', my_input['body'], re.IGNORECASE):
+if re.match(r'THIS IS A TEST.', input_data['body'], re.IGNORECASE):
     output['test_worked'] = True
-    tmp = re.search(r'(THIS IS A TEST.)([^\.]*)', my_input['body'], re.IGNORECASE)
+    tmp = re.search(r'(THIS IS A TEST.)([^\.]*)', input_data['body'], re.IGNORECASE)
     output['insert_text'] = tmp.group(2)
