7ac87f24126696808b6f26ae90a4d5cbde1b52c8
Rick Henry <fredericmhenry@gmail.com>
2017-07-16 17:40:55 -0400

    multiline search
    Signed-off-by: Rick Henry <fredericmhenry@gmail.com>

--- parse_text.py
+++ parse_text.py
@@ -6,11 +6,12 @@ input_data = { 'body': 'this is a test. This is the text to insert.',
 # COPY THIS CODE INTO ZAPIER
 output = { 'matched': False, 'insert_text': '' }
 # replace these values to customize. See README for details.
-trigger_phrase = r'(THIS IS A TEST.)' 
+trigger_phrase = r'(THIS IS A TEST. )' # include a space at the end or the
+                                       # found text will start with a space
 term_char = r'([^\.]*)' 
 
 import re
-if re.match(trigger_phrase, input_data['body'], re.IGNORECASE):
+if re.search(trigger_phrase, input_data['body'], re.IGNORECASE):
     output['matched'] = True
     long_search = trigger_phrase + r'([^\.]*)'
     tmp = re.search(long_search, input_data['body'], re.IGNORECASE)
